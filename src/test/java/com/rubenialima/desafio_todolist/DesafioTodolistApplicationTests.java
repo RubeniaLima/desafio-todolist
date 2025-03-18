8a23cdc33e249bae3c6ca2d1d459e9d1434e82cf
Rubenia M Lima <rubenia.lima2608@gmail.com>
2025-03-18 00:11:36 -0300

    Implementando a Classe de Testes

--- src/test/java/com/rubenialima/desafio_todolist/DesafioTodolistApplicationTests.java
+++ src/test/java/com/rubenialima/desafio_todolist/DesafioTodolistApplicationTests.java
@@ -1,13 +1,38 @@
 package com.rubenialima.desafio_todolist;
 
+import com.rubenialima.desafio_todolist.entity.Todo;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DesafioTodolistApplicationTests {
 
+	@Autowired
+	private WebTestClient webTestClient;
+
 	@Test
-	void contextLoads() {
+	void testeCreateTodoSuccess() {
+		var todo= new Todo("task 01","desc todo list01", false, 1);
+
+		webTestClient
+				.post()
+				.uri("/todos")
+				.bodyValue(todo)
+				.exchange()
+				.expectStatus().isCreated()
+				.expectBody()
+				.jsonPath("$").isArray()
+				.jsonPath("$.length()").isEqualTo(1)
+				.jsonPath("$[0].nome").isEqualTo(todo.getNome())
+				.jsonPath("$[0].descricao").isEqualTo(todo.getDescricao())
+				.jsonPath("$[0].realizado").isEqualTo(todo.isRealizado())
+				.jsonPath("$[0].prioridade").isEqualTo(todo.getPrioridade());
+	}
+
+	@Test
+	void testeCreateTodoFailure() {
 	}
 
 }
